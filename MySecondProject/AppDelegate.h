b60d971a93b7974d105281349cd5e6e7a752695a
Shobha V J <shobha@Shobhas-Mac-mini.local>
2017-08-21 23:23:31 -0400

    Initial Commit

---
+++ MySecondProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MySecondProject
+//
+//  Created by Shobha V J on 21/8/2017.
+//  Copyright © 2017 Shobha V J. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MySecondProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MySecondProject
+//
+//  Created by Shobha V J on 21/8/2017.
+//  Copyright © 2017 Shobha V J. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
